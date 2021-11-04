82d112e262a64af3980b02d1d2e310a6402f004a
Evgenij009 <evgeshashadura@gmail.com>
2021-11-04 23:20:39 +0300

    fix tests

--- TestAssemblyInfoGetter/AssemblyInfoResult.cs
+++ TestAssemblyInfoGetter/AssemblyInfoResult.cs
@@ -30,7 +30,7 @@ namespace TestAssemblyInfoGetter
             infoGetter.LoadAssembly("TestAssemblyInfoGetter.dll");
             var result = infoGetter.GetAssemblyInfo();
 
-            Assert.AreEqual("AssemblyInfoResult", result.DataTypes[0].Name);
+            Assert.AreEqual("AssemblyInfoResult", result.Namespaces[0].DataTypes[0].Name);
         }
 
         [TestMethod]
@@ -41,7 +41,7 @@ namespace TestAssemblyInfoGetter
             infoGetter.LoadAssembly("TestAssemblyInfoGetter.dll");
             var result = infoGetter.GetAssemblyInfo();
 
-            Assert.AreEqual("TestAssemblyName", result.DataTypes[0].Methods[0].Name);
+            Assert.AreEqual("TestAssemblyName", result.Namespaces[0].DataTypes[0].Methods[0].Name);
         }
 
         [TestMethod]
@@ -52,7 +52,7 @@ namespace TestAssemblyInfoGetter
             infoGetter.LoadAssembly("TestAssemblyInfoGetter.dll");
             var result = infoGetter.GetAssemblyInfo();
 
-            Assert.AreEqual("Num", result.DataTypes[0].Fields[0].Name);
+            Assert.AreEqual("Num", result.Namespaces[0].DataTypes[0].Fields[0].Name);
         }
 
         [TestMethod]
@@ -63,7 +63,7 @@ namespace TestAssemblyInfoGetter
             infoGetter.LoadAssembly("TestAssemblyInfoGetter.dll");
             var result = infoGetter.GetAssemblyInfo();
 
-            Assert.AreEqual(typeof(int), result.DataTypes[0].Fields[0].Type);
+            Assert.AreEqual(typeof(int), result.Namespaces[0].DataTypes[0].Fields[0].Type);
         }
     }
 }
